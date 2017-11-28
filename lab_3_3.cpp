c8c5c91015f66c4535efa05d3b7a6ece22e09bfe
Unknown <sassytofu@gmail.com>
2017-11-28 11:50:43 +0700

    ข้อ3

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main() {
-  
+
+  const float pi = 3.14315926536;
+  float r;
+  int k = 2 ,h = 15;
+  cout <<"Please enter radius : ";
+  cin >> r;
+  cout <<"Area of Circle : "<<pi*r*r<<endl;
+
+  cout <<"Circumference of Circle : "<<k*pi*r<<endl;
+
+  cout <<"Cylindrical space : "<<pi*(r*r)*h<<endl;
   return 0;
 }
