c306e28b349894f6aec24f6a1361b96406c15594
Unknown <sassytofu@gmail.com>
2017-11-28 12:14:58 +0700

    ข้อ1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+  cout << "Mr." << endl;
+  cout << "Paran" << endl;
+  cout << "Wongplub" << endl;
+  cout << "Win" << endl;
+  cout << "man" << endl;
+  cout << "12 August 1999" << endl;
+  cout << "play baskeball" << endl;
+  cout << "single" << endl;
+
   return 0;
 }
